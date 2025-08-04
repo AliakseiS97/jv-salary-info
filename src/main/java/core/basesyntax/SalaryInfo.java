00dcae83e8c1e36b5914ce96243b4e8a1fdcea1c
AliakseiS97 <aliakseis97@gmail.com>
2025-08-04 03:34:18 +0200

    Salary. First solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,50 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startMons = LocalDate.parse(dateFrom, formatter);
+        LocalDate endMonths = LocalDate.parse(dateTo, formatter);
+
+        for (String worker : names) {
+            int salary = 0;
+
+            for (String dataIndex : data) {
+                String dataStr = dataIndex.split(" ")[0];
+                String workerName = dataIndex.split(" ")[1];
+                String workHour = dataIndex.split(" ")[2];
+                String salaryPerHour = dataIndex.split(" ")[3];
+
+                LocalDate workingDay = LocalDate.parse(dataStr, formatter);
+
+                if ((worker.equals(workerName))
+                        && (!workingDay.isBefore(startMons))
+                        && (!workingDay.isAfter(endMonths))) {
+                    salary += Integer.parseInt(workHour) * Integer.parseInt(salaryPerHour);
+                } else {
+                    System.out.println("Check data!");
+                }
+            }
+            report.append(worker)
+                    .append(" - ")
+                    .append(salary);
+
+            if (!worker.equals(names[names.length - 1])) {
+                report.append(System.lineSeparator());
+            }
+        }
+
+        return report.toString();
     }
 }
